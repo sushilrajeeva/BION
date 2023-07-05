98df416495be1e80145ef3d8212d1108edda7cf9
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-07-05 06:37:45 +0530

    Customer Registration with XSS support added
    Customer registration support is added
    XSS has been implemented to sanitize the http request inputs
    upon successful validations the user is added to customerCollection in mongo and appropriate success message is sent in routes.

--- backend/model/customer.js
+++ backend/model/customer.js
@@ -1,13 +1,107 @@
 import { Router } from "express";
 import { ObjectId } from "mongodb";
-import { primaryUsers } from "../config/mongoCollections.js";
-import { listings } from "../config/mongoCollections.js";
-import { scoutUsers } from "../config/mongoCollections.js";
-import { messages } from "../config/mongoCollections.js";
-import helpers from "../helpers.js";
-
 import bcrypt from "bcryptjs";
+import Joi from "joi";
+import { customerCollection } from "../config/mongoCollections.js";
 
 const saltRounds = 12;
 
 const router = Router();
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  emailAddress: Joi.string().email().required(),
+  countryCode: Joi.string().required(),
+  phoneNumber: Joi.string().required(),
+  address: Joi.string().required(),
+  city: Joi.string().required(),
+  state: Joi.string().required(),
+  pinCode: Joi.string().required(),
+  country: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+export const createCustomer = async (
+  name,
+  emailAddress,
+  countryCode,
+  phoneNumber,
+  address,
+  city,
+  state,
+  pinCode,
+  country,
+  password
+) => {
+  console.log("Create Customer Function Model is called!");
+
+  try {
+    let custData = {
+      name: name,
+      emailAddress: emailAddress,
+      countryCode: countryCode,
+      phoneNumber: phoneNumber,
+      address: address,
+      city: city,
+      state: state,
+      pinCode: pinCode,
+      country: country,
+      password: password,
+    };
+
+    // Validate the data
+    const validationResult = userSchema.validate(custData);
+    if (validationResult.error) {
+      throw new Error(validationResult.error.details[0].message);
+    }
+
+    const hashedPassword = await bcrypt.hash(custData.password, saltRounds);
+    const custCol = await customerCollection();
+
+    const custUser = await custCol.findOne({
+      emailAddress: custData.emailAddress,
+    });
+
+    if (custUser) {
+      throw `The email address - ${adminData.emailAddress} that you used to register as a customer user, already exist! Please login!`;
+    }
+
+    let custUserData = {
+      _id: new ObjectId(),
+      name: custData.name,
+      emailAddress: custData.emailAddress,
+      countryCode: custData.countryCode,
+      phoneNumber: custData.phoneNumber,
+      address: custData.address,
+      city: custData.city,
+      state: custData.state,
+      pinCode: custData.pinCode,
+      country: custData.country,
+      password: hashedPassword,
+    };
+
+    const insertInfo = await custCol.insertOne(custUserData);
+
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+      throw "Could not add customer!!";
+    }
+
+    return { insertedUser: true };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getCustomer = async (emailAddress) => {
+  try {
+    const custCol = await customerCollection();
+    const custUser = await custCol.findOne({ emailAddress });
+
+    // If findOne returns null (no adminUser found), it means the admin doesn't exist
+    return custUser !== null;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default { createCustomer, getCustomer };
--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs";
 
 import xss from "xss";
 import admin from "../model/admin.js";
+import customer from "../model/customer.js";
 
 router.route("/").get(async (req, res) => {
   //code here for GET THIS ROUTE SHOULD NEVER FIRE BECAUSE OF MIDDLEWARE #1 IN SPECS.
@@ -32,14 +33,60 @@ router.route("/register").post(async (req, res) => {
 
     console.log("Register user recieved -> ", req.body);
 
+    let name = xss(req.body.name);
+    let emailAddress = xss(req.body.emailAddress);
+    let countryCode = xss(req.body.countryCode);
+    let phoneNumber = xss(req.body.phoneNumber);
+    let address = xss(req.body.address);
+    let city = xss(req.body.city);
+    let state = xss(req.body.state);
+    let pinCode = xss(req.body.pinCode);
+    let country = xss(req.body.country);
+    let password = xss(req.body.password);
+
+    console.log("Sanitized inputs -> ", {
+      name: name,
+      emailAddress: emailAddress,
+      countryCode: countryCode,
+      phoneNumber: phoneNumber,
+      address: address,
+      city: city,
+      state: state,
+      pinCode: pinCode,
+      country: country,
+      password: password,
+    });
+
     // Consider not returning the entire user object for privacy/security
     // Consider returning only the needed data
-    res.status(200).json({ adminUser: "Register User Created!" });
+    const createdCustomer = await customer.createCustomer(
+      name,
+      emailAddress,
+      countryCode,
+      phoneNumber,
+      address,
+      city,
+      state,
+      pinCode,
+      country,
+      password
+    );
+
+    if (createdCustomer.insertedUser === true) {
+      //if this condition is valid i will load my login page
+      //console.log("Created user validation hit");
+
+      console.log("User created!");
+
+      //using 201 instead of 200 to give more accurate description
+      return res.status(201).json({ Success: "Register User Created!" });
+    } else {
+      //console.log("Didn't blow up !!");
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while registering the admin" });
+    res.status(500).json({ error: error });
   }
 });
 
