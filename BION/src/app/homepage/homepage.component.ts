d435a182dc79fee69b2f468a9e94c257263c48d6
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-07-09 00:52:32 +0530

    Updating navbar and homepage

--- BION/src/app/homepage/homepage.component.ts
+++ BION/src/app/homepage/homepage.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  products = [
+    {
+      name: 'Product 1',
+      description: 'This is product 1',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      name: 'Product 2',
+      description: 'This is product 2',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      name: 'Product 3',
+      description: 'This is product 3',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      name: 'Product 4',
+      description: 'This is product 4',
+      image: 'https://via.placeholder.com/150',
+    },
+    // Add more products as needed
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
